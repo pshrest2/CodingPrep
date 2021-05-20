935415d3ad9136605699f44f5ac27b5388f25007
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-20 14:27:53 -0500

    insert function added

--- problems/linkedList.js
+++ problems/linkedList.js
@@ -16,7 +16,6 @@ class LinkedList {
   }
 
   append(value) {
-    //Code here
     const newNode = new Node(value);
 
     this.tail.next = newNode;
@@ -31,11 +30,41 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      console.log("Index out of bound! Adding at the end of the list");
+      this.append(value);
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    const newNode = new Node(value);
+    let currentNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next;
+    }
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+    this.length++;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(18);
+myLinkedList.insert(20, 96);
 
-console.log(myLinkedList);
+myLinkedList.printList();
